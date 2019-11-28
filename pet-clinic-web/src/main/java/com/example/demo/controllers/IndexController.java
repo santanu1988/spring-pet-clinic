57c18dab7d9c214780bd67d831e201e2d10606c6
Santanu Santra <ssantra@aienterprise.com>
2019-11-28 18:56:38 +0530

    Adding some th text for test

--- pet-clinic-web/src/main/java/com/example/demo/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 	
 	@RequestMapping({"","/","index","index.html"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("extraInfo", "This is a test text.");
 		return "index";
 	}
 

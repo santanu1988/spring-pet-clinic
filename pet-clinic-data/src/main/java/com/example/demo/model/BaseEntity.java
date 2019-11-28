2bd3a1f4c2c1d4d1b669a2fdc6afc9c50fd461b8
Santanu Santra <ssantra@aienterprise.com>
2019-11-28 15:39:29 +0530

    Added BaseEntity

---
+++ pet-clinic-data/src/main/java/com/example/demo/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+	
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+}

a01b6a0ea940d404497ec5f1d15f76b156caef86
Santanu Santra <ssantra@aienterprise.com>
2019-11-29 16:07:32 +0530

    Putting auto generated id logic

--- pet-clinic-data/src/main/java/com/example/demo/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/BaseEntity.java
@@ -4,13 +4,13 @@ import java.io.Serializable;
 
 public class BaseEntity implements Serializable {
 	
-	private long id;
+	private Long id;
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	
--- pet-clinic-data/src/main/java/com/example/demo/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/demo/services/map/AbstractMapService.java
@@ -1,13 +1,17 @@
 package com.example.demo.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+import com.example.demo.model.BaseEntity;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 	
-	protected Map<ID, T> map = new HashMap<>();
+	protected Map<Long, T> map = new HashMap<>();
 	
 	Set<T> findAll() {
 		return new HashSet<>(map.values());
@@ -17,8 +21,15 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 	
-	T save(ID id, T object) {
-		map.put(id, object);
+	T save(T object) {
+		if(object != null) {
+			if(object.getId() == null) {
+				object.setId(getNextId());
+			}
+		} else {
+			throw new RuntimeException("Object cannot be null");
+		}
+		map.put(object.getId(), object);
 		return object;
 	}
 	
@@ -28,6 +39,16 @@ public abstract class AbstractMapService<T, ID> {
 	
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+	
+	private Long getNextId() {
+		Long nextId = null;
+		try {
+			nextId = Collections.max(map.keySet()) + 1;
+		} catch (NoSuchElementException e) {
+			nextId = 1L;
+		}
+		return nextId;
 	}
 
 }

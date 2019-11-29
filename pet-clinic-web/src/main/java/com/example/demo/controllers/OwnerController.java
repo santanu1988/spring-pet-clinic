6b34d85360a66c6256468357ff6b545e68d6f40d
Santanu Santra <ssantra@aienterprise.com>
2019-11-29 15:33:48 +0530

    Showing owner and vets on index page from map data

--- pet-clinic-web/src/main/java/com/example/demo/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/demo/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"","/","/index","/index.html"})
-	public String listVets() {
+	public String listOwners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/ownerindex";
 	}
 

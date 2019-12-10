39fdf6c7de3c2ed6ad42d7e9715a730d9581fb3b
Santanu Santra <santanu1988.s@gmail.com>
2019-12-11 01:13:43 +0530

    Added required repository interfaces

---
+++ pet-clinic-data/src/main/java/com/example/demo/repositories/VisitRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Visit;
+
+public interface VisitRepository extends CrudRepository<Visit, Long> {
+
+}

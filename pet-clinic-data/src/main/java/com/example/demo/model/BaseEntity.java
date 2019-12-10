d108f4fcfab77f47adc48596d9ba51eaa4f0f4dd
Santanu Santra <santanu1988.s@gmail.com>
2019-12-10 19:50:36 +0530

    Updated BaseEntity with JPA annotation

--- pet-clinic-data/src/main/java/com/example/demo/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/BaseEntity.java
@@ -2,8 +2,16 @@ package com.example.demo.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass /** This annotation is used to inform JPA that this is a class to be inherited by other entities, no need map this class to the database. */
 public class BaseEntity implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {

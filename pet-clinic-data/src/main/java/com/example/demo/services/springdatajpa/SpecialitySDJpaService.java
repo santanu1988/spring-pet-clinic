112c370f6e48030daa2f6d7fa920aede77995d9d
Santanu Santra <santanu1988.s@gmail.com>
2019-12-11 02:15:57 +0530

    Adding Speciality JPA service

---
+++ pet-clinic-data/src/main/java/com/example/demo/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,50 @@
+package com.example.demo.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Speciality;
+import com.example.demo.repositories.SpecialityRepository;
+import com.example.demo.services.SpecialityService;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+	
+	private final SpecialityRepository specialityRepository;
+
+	public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+		this.specialityRepository = specialityRepository;
+	}
+
+	@Override
+	public Set<Speciality> findAll() {
+		Set<Speciality> specialities = new HashSet<>();
+		specialityRepository.findAll().forEach(specialities::add);
+		return specialities;
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return specialityRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		return specialityRepository.save(object);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		specialityRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		specialityRepository.deleteById(id);
+	}
+
+}

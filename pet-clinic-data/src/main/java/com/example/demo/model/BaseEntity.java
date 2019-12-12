53cf41e1f3829586097b39b38ba0dc85b2a151ed
Santanu Santra <santanu1988.s@gmail.com>
2019-12-12 15:16:50 +0530

    Adding project lombok

--- pet-clinic-data/src/main/java/com/example/demo/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/BaseEntity.java
@@ -7,6 +7,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass /** This annotation is used to inform JPA that this is a class to be inherited by other entities, no need map this class to the database. */
 public class BaseEntity implements Serializable {
 	
@@ -14,12 +23,4 @@ public class BaseEntity implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
 }
--- pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
@@ -10,6 +10,17 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -18,13 +29,5 @@ public class Vet extends Person {
 	@JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), 
 			inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities = new HashSet<>();
-
-	public Set<Speciality> getSpecialities() {
-		return specialities;
-	}
-
-	public void setSpecialities(Set<Speciality> specialities) {
-		this.specialities = specialities;
-	}
 
 }

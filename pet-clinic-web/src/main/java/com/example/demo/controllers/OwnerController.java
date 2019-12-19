0d0c96557bb42e95d4fb22811a67468e9b61f56b
Santanu Santra <santanu1988.s@gmail.com>
2019-12-19 17:13:39 +0530

    Adding owner details page

--- pet-clinic-web/src/main/java/com/example/demo/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/demo/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.services.OwnerService;
 
@@ -25,6 +28,13 @@ public class OwnerController {
 	@RequestMapping("/find")
 	public String findOwners() {
 		return "notimplemented";
+	}
+	
+	@GetMapping("/{ownerId}")
+	public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+		ModelAndView mav = new ModelAndView("owners/ownerDetails");
+		mav.addObject(this.ownerService.findById(ownerId));
+		return mav;
 	}
 
 }

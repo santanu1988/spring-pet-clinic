cd283ff5da5b5767502de14fdc2957bbeb018fd9
Santanu Santra <santanu1988.s@gmail.com>
2019-12-30 15:48:00 +0530

    Added get vets controller

--- pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
@@ -20,10 +20,15 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
+	
+	@Builder
+	public Vet(Long id, String firstName, String lastName, Set<Speciality> specialities) {
+		super(id, firstName, lastName);
+		this.specialities = specialities;
+	}
 	
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), 
--- pet-clinic-web/src/main/java/com/example/demo/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/demo/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.model.Vet;
 import com.example.demo.services.VetService;
 
 @Controller
@@ -19,6 +24,11 @@ public class VetController {
 	public String listVets(Model model) {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/vetindex";
+	}
+	
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetJson() {
+		return vetService.findAll();
 	}
 
 }
